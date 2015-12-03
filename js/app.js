7f928f068db2a48602d214ab27371350d799d19b
Alan Wright <alanwright47@gmail.com>
2015-12-02 21:25:41 -0800

    testing base tag

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 'use strict';
 
-angular.module('app', ['app.controllers', 'app.directives']);
+angular.module('app', ['app.controllers', 'app.directives']).config(function ($locationProvider) {
+  $locationProvider.html5Mode(true);
+});
