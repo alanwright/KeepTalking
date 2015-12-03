728a2a9cb05ee16630e2ab8c7d2d42f75d06fe8d
Alan Wright <alanwright47@gmail.com>
2015-12-02 21:29:24 -0800

    remove base tag

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
 'use strict';
 
-angular.module('app', ['app.controllers', 'app.directives']).config(function ($locationProvider) {
-  $locationProvider.html5Mode(true);
-});
+angular.module('app', ['app.controllers', 'app.directives']);
